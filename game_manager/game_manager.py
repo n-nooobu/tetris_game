a76d7e85b08760427ad60bc3339e5ec690a713dc
n-nooobu <kamiya826@gmail.com>
2021-07-03 16:22:11 +0900

    commit

--- game_manager/game_manager.py
+++ game_manager/game_manager.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
 
 from board_manager import BOARD_DATA, Shape
-from block_controller import BLOCK_CONTROLLER
+from block_controller_random import BLOCK_CONTROLLER
 from block_controller_sample import BLOCK_CONTROLLER_SAMPLE
 
 from argparse import ArgumentParser
